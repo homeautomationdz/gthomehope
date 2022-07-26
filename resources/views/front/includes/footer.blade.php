05f4709670c76eac2ffcbfd21cf475e08ce1be4c
homeautomationdz <108092263+homeautomationdz@users.noreply.github.com>
2022-07-26 14:11:49 +0100

    first commit

---
+++ resources/views/front/includes/footer.blade.php
@@ -0,0 +1,157 @@
+<!-- Footer start -->
+<footer class="footer-5">
+    <div class="lines">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+    <div class="subscribe-newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <h3>Subscribe Newsletter</h3>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="Subscribe-box">
+                        <div class="newsletter-content-wrap">
+                            <form class="newsletter-form d-flex" action="#">
+                                <input class="form-control" type="email" id="email" placeholder="Email Address...">
+                                <button class="btn btn-submit" type="submit"><i class="fa fa-paper-plane"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="footer-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-8 col-sm-6">
+                    <div class="footer-item clearfix">
+                        <h4>
+                            Contact Info
+                        </h4>
+                        <div class="s-border"></div>
+                        <div class="m-border"></div>
+                        <ul class="contact-info">
+                            <li>
+                                <i class="flaticon-facebook-placeholder-for-locate-places-on-maps"></i> 360 Harvest St, North Subract, London. United States Of Amrica.
+                            </li>
+                            <li>
+                                <i class="fa fa-envelope"></i><a href="mailto:sales@hotelempire.com"> info@themevessel.com</a>
+                            </li>
+                            <li>
+                                <i class="fa fa-phone"></i><a href="tel:+55-417-634-7071"> +0477 85X6 552</a>
+                            </li>
+                            <li class="mb-0">
+                                <i class="fa fa-fax"></i>+0477 85X6 552
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6">
+                    <div class="footer-item">
+                        <h4>
+                            Useful Links
+                        </h4>
+                        <div class="s-border"></div>
+                        <div class="m-border"></div>
+                        <ul class="links">
+                            <li>
+                                <a href="#"><i class="fa fa-angle-right"></i>Home</a>
+                            </li>
+                            <li>
+                                <a href="about.html"><i class="fa fa-angle-right"></i>About Us</a>
+                            </li>
+                            <li>
+                                <a href="services.html"><i class="fa fa-angle-right"></i>Services</a>
+                            </li>
+                            <li>
+                                <a href="blog-classic-sidebar-right.html"><i class="fa fa-angle-right"></i>Blog</a>
+                            </li>
+                            <li>
+                                <a href="dashboard.html"><i class="fa fa-angle-right"></i>Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="contact.html"><i class="fa fa-angle-right"></i>Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="elements.html"><i class="fa fa-angle-right"></i>Elements</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                    <div class="footer-item fi-2 clearfix">
+                        <div class="footer-gallery clearfix">
+                            <h4>Our Gallery</h4>
+                            <div class="s-border"></div>
+                            <div class="m-border"></div>
+                            <ul>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property.png" alt="small-img">
+                                </li>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property-2.png" alt="small-img">
+                                </li>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property-3.png" alt="small-img">
+                                </li>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property-4.png" alt="small-img">
+                                </li>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property-5.png" alt="small-img">
+                                </li>
+                                <li>
+                                    <img src="assets/img/sub-property/sub-property-6.png" alt="small-img">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <p class="copy">© 2022 <a href="#">Theme Vessel.</a> All Rights Reserved.</p>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="social-media clearfix">
+                        <div class="social-list">
+                            <div class="icon facebook">
+                                <div class="tooltip">Facebook</div>
+                                <span><i class="fa fa-facebook"></i></span>
+                            </div>
+                            <div class="icon twitter">
+                                <div class="tooltip">Twitter</div>
+                                <span><i class="fa fa-twitter"></i></span>
+                            </div>
+                            <div class="icon instagram">
+                                <div class="tooltip">Instagram</div>
+                                <span><i class="fa fa-instagram"></i></span>
+                            </div>
+                            <div class="icon github">
+                                <div class="tooltip">Github</div>
+                                <span><i class="fa fa-github"></i></span>
+                            </div>
+                            <div class="icon youtube mr-0">
+                                <div class="tooltip">Youtube</div>
+                                <span><i class="fa fa-youtube"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer end -->
